fa659bd10c5f2e17124ded6a0084f219b1ac4b60
Karl WIlvers <karl@wilvers.net>
2019-04-24 17:08:01 +0200

    metadata

---
+++ CancerCellDetection/ImageProcessingTests/MachineLearning/CellCountAndIdentify.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImageProcessingTests.MachineLearning
+{
+    [TestClass]
+    public class CellCountAndIdentify
+    {
+        [TestMethod]
+        public void GetMetadata()
+        {
+            Image image = new Bitmap(@".\metadata.png");
+            // Get the PropertyItems property from image.
+            var propItems = image.PropertyItems;
+            int count = 0;
+
+            foreach (var propItem in propItems)
+            {
+                Console.WriteLine("Property Item " + count.ToString());
+                Console.WriteLine("   iD: 0x" + propItem.Id.ToString("x"));
+                Console.WriteLine("   type: " + propItem.Type.ToString());
+                Console.WriteLine("   length: " + propItem.Len.ToString() + " bytes");
+
+                count++;
+            }
+        }
+    }
+}
